194fec8f8c087af7c157201d627c7c310aca45fd
Aaron Sachs <aaronm.sachs@gmail.com>
2018-05-29 21:43:21 -0500

    Updating provisioning script & adding graphite install script

--- sensu-provisioning-script.sh
+++ sensu-provisioning-script.sh
@@ -2,7 +2,8 @@
 IPADDR=$(/sbin/ip -o -4 addr list enp0s8  | awk '{print $4}' | cut -d/ -f1)
 
 # Make sure we have all the package repos we need!
- sudo yum install epel-release vim yum-utils openssl -y
+sudo yum install epel-release vim yum-utils openssl -y
+sudo yum groupinstall 'Development Tools' -y
 
 # Set up zero-dependency erlang
 echo ' [rabbitmq-erlang]
@@ -55,6 +56,16 @@ enabled=1' | sudo tee /etc/yum.repos.d/sensu.repo
  sudo yum install curl jq -y
 
 # Provide minimal uchiwa conifguration, pointing at API on localhost
+# Optionally, you can see Sensu datacenters(see https://docs.uchiwa.io/getting-started/configuration/#datacenters-configuration-sensu) in action by adding an additional 
+# configuration for another datacenter. If you, by chance, spin up Sensu using 
+# kubernetes, it might look like this:
+
+#    {                
+#      "name": "sensu-k8s",                     
+#      "host": "your-minikube-ip",                 
+#      "port": your-minikube-service-port
+#    }
+
   echo '{
   "sensu": [
     {
@@ -99,6 +110,64 @@ sudo systemctl start rabbitmq-server
 sudo rabbitmqctl add_vhost /sensu
 sudo rabbitmqctl add_user sensu secret
 sudo rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
+
+# Going to do some general setup stuff
+cd /etc/sensu/conf.d
+mkdir {checks,filters,mutators,handlers,templates}
+
+# Installing some Nagios & Sensu plugins
+sudo yum install -y nagios-plugins nagios-plugins-disk
+
+sudo yum update -y
+
+sudo sensu-install -p sensu-plugins-network-checks
+sudo sensu-install -p sensu-plugins-graphite
+sudo sensu-install -p sensu-plugins-http
+sudo sensu-install -p sensu-plugins-mailer
+sudo sensu-install -e snmp-trap:0.0.33
+
+# Creating a some building blocks
+# Disk check - Uses the nagios-disk plugin 
+echo '{
+  "checks": {
+    "fs_free_space": {
+      "command": "/usr/lib64/nagios/plugins/check_disk  -w 90 -c 95",
+      "subscribers": ["dev"],
+      "interval": 30
+    }
+  }
+}' | sudo tee /etc/sensu/conf.d/checks/check_disk.json
+
+# Filter - 
+
+# Mutator - 
+
+# Handlers 
+# Using the community 'Mailer' plugin (https://github.com/sensu-plugins/sensu-plugins-mailer)
+echo '{
+    "mailer" :  {
+        "admin_gui" : "http://localhost:3000",
+        "mail_from": "from@email.com",
+        "mail_to": "to@email.com",
+        "delivery_method": "smtp",
+        "smtp_address": "localhost",
+        "smtp_port": "25",
+        "smtp_domain": "localhost.local_domain",
+        "smtp_enable_starttls_auto": "true",
+        "smtp_username" : "username",
+        "smtp_password" : "XXXXXXXX"
+    },  
+    "handlers": {
+        "mailer": {
+            "type": "pipe",
+            "command": "/opt/sensu/embedded/bin/handler-mailer.rb"
+        }
+    }
+}' | sudo tee /etc/sensu/conf.d/handlers/mailer.json
+
+# Using the community 'Graphite' handler
+echo '' | sudo tee /etc/sensu/conf.d/handles/graphite
+
 
 #Start up other services
 sudo systemctl start sensu-{server,api,client}.service
